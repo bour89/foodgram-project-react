1eb3c6087933ec10edf80b54b08aaab1ce145cc5
Артём Тимашков <bour@yandex.ru>
2023-02-05 13:56:18 +0300

    Fix after 3rd deploy review

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -99,11 +99,9 @@ class RecipePostSerializer(serializers.ModelSerializer):
                 )
             ingredient_list.append(ingredient)
             if int(ingredient_item['amount']) < 0:
-                raise serializers.ValidationError({
-                    'ingredients': (
-                        'Убедитесь, что количество ингредиентов больше 0'
-                    )
-                })
+                raise serializers.ValidationError(
+                    'Убедитесь, что количество ингредиентов больше 0'
+                )
         data['ingredients'] = ingredients
         return data
 
