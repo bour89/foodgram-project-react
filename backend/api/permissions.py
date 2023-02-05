02ec07d57caa088b36a69b1bc47b88a6304be6ed
Артём Тимашков <bour@yandex.ru>
2023-02-05 09:04:37 +0300

    Fix after 2nd deploy review

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,12 +2,5 @@ from rest_framework import permissions
 
 
 class IsAuthorOrAdminPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (request.user.is_authenticated
-                or request.method in permissions.SAFE_METHODS)
-
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or (request.user and request.user.is_authenticated
-                    and (request.user == obj.author
-                         or request.user.is_superuser)))
+        return request.user.is_staff or obj.author == request.user
